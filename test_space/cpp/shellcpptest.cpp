834c8420c04d1950e4c2d42bbe7df86542b316a5
ishitaku5522 <ishitaku5522@gmail.com>
2016-12-26 19:10:46 +0900

    change testcode

--- test_space/cpp/shellcpptest.cpp
+++ test_space/cpp/shellcpptest.cpp
@@ -1,17 +1,29 @@
+#include <unistd.h>
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
-#include <cstdlib>
-#include <unistd.h>
 
-int main(int argc, char* argv[]){
-    bool result = false;
-    int return_code = -1;
+char* getmodulefilename() {
+    static char buf[1024] = {};
+    /*
+        char path[256];
+        sprintf( path, "/proc/%d/exe", getpid() );
+        readlink( path, buf, sizeof(buf)-1 );
+    */
+    readlink("/proc/self/exe", buf, sizeof(buf) - 1);
+    return buf;
+}
+
+int main(int argc, char* argv[]) {
+    bool              result      = false;
+    int               return_code = -1;
     std::stringstream ss;
-    ss << "gnome-terminal -e " << "./test";
+    ss << "gnome-terminal -e "
+       << "./test";
 
     return_code = system(ss.str().c_str());
     std::cout << "return_code_before" << return_code << std::endl;
-    return_code = WEXITSTATUS(return_code); // ①
+    return_code = WEXITSTATUS(return_code);  // ①
     std::cout << "return_code_after" << return_code << std::endl;
 
     if (return_code == 0) {
