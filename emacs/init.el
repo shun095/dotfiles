159f76f75ec8a6bc3d5daee6102fce2e96fc5cbc
ishitaku5522 <ishitaku5522@gmail.com>
2017-04-19 04:05:04 +0900

    change emacs settings

--- emacs/init.el
+++ emacs/init.el
@@ -3,21 +3,41 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
 (require 'linum)            ;\左に行番号表示
 (global-linum-mode)
 (setq line-number-display-limit-width 10000)
+
 (load-theme 'tango-dark t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages (quote (auto-complete))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ricty Diminished for Powerline" :foundry "PfEd" :slant normal :weight normal :height 128 :width normal)))))
+;;
+;; Auto Complete
+;;
+;; auto-complete-config の設定ファイルを読み込む。
+(require 'auto-complete-config)
+
+;; よくわからない
+(ac-config-default)
+
+;; TABキーで自動補完を有効にする
+(ac-set-trigger-key "TAB")
+
+;; auto-complete-mode を起動時に有効にする
+(global-auto-complete-mode t)
