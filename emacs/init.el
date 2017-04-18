676b617bb7fcb599bb3ddef66dbaec6677f36b00
ishitaku5522 <ishitaku5522@gmail.com>
2017-04-19 03:13:39 +0900

    change color theme(emacs)

--- emacs/init.el
+++ emacs/init.el
@@ -8,3 +8,16 @@
 (require 'linum)            ;\左に行番号表示
 (global-linum-mode)
 (setq line-number-display-limit-width 10000)
+(load-theme 'tango-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ricty Diminished for Powerline" :foundry "PfEd" :slant normal :weight normal :height 128 :width normal)))))
