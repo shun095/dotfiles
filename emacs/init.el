ba6a707c9fb984c4866af75792818f6c9a7c6f73
ishitaku5522 <ishitaku5522@gmail.com>
2017-04-19 05:11:49 +0900

    larger emacs

--- emacs/init.el
+++ emacs/init.el
@@ -17,6 +17,7 @@
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
+(setq inhibit-startup-message t)
 
 (require 'linum)            ;\左に行番号表示
 (global-linum-mode)
@@ -51,3 +52,21 @@
 
 ;; auto-complete-mode を起動時に有効にする
 (global-auto-complete-mode t)
+
+(defvar eww-disable-colorize t)
+(defun shr-colorize-region--disable (orig start end fg &optional bg &rest _)
+  (unless eww-disable-colorize
+    (funcall orig start end fg)))
+(advice-add 'shr-colorize-region :around 'shr-colorize-region--disable)
+(advice-add 'eww-colorize-region :around 'shr-colorize-region--disable)
+(defun eww-disable-color ()
+  "eww で文字色を反映させない"
+  (interactive)
+  (setq-local eww-disable-colorize t)
+  (eww-reload))
+(defun eww-enable-color ()
+  "eww で文字色を反映させる"
+  (interactive)
+  (setq-local eww-disable-colorize nil)
+  (eww-reload))
+(setq eww-search-prefix "http://www.google.co.jp/search?q=")
