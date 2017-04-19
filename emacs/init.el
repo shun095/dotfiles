e69c47de6191ec7196dfb9cd20f16f196d9fe7eb
ishitaku5522 <ishitaku5522@gmail.com>
2017-04-19 12:51:30 +0900

    emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,10 @@
-(set-language-environment 'Japanese) 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (defvar my-favorite-package-list
   '(undo-tree
+    helm
     auto-complete)
   "packages to be installed")
 
@@ -23,8 +23,6 @@
 (global-linum-mode)
 (setq line-number-display-limit-width 10000)
 
-(load-theme 'tango-dark t)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,12 +30,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (auto-complete))))
 
-(custom-set-faces
+;;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ricty Diminished for Powerline" :foundry "PfEd" :slant normal :weight normal :height 128 :width normal)))))
+;; '(default ((t (:family "Ricty Diminished for Powerline" :foundry "PfEd" :slant normal :weight normal :height 128 :width normal)))))
 ;;
 ;; Auto Complete
 ;;
@@ -70,3 +68,9 @@
   (setq-local eww-disable-colorize nil)
   (eww-reload))
 (setq eww-search-prefix "http://www.google.co.jp/search?q=")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
