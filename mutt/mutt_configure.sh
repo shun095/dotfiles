825557d0d3d53f4e032b22106e1131e6ae3ff861
ishitaku5522 <ishitaku5522@gmail.com>
2017-08-05 01:48:38 +0900

    add mutt configure

---
+++ mutt/mutt_configure.sh
@@ -0,0 +1,18 @@
+./configure --enable-dependency-tracking \
+    --enable-sidebar \
+    --enable-compressed \
+    --enable-external-dotlock \
+    --enable-pop \
+    --enable-imap \
+    --enable-smtp \
+    --enable-nfs-fix \
+    --enable-mailtool \
+    --enable-locales-fix \
+    --enable-exact-address \
+    --enable-hcache \
+    --with-ssl \
+    --with-regex \
+    --with-gnutls \
+    --with-sasl \
+    --with-included-gettext \
+    --with-idn
