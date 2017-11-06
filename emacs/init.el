2d77bee86bcf627caedaa17c61be7112bf98c42f
ishitaku5522 <ishitaku5522@gmail.com>
2017-11-07 03:27:19 +0900

    emacs fix

--- emacs/init.el
+++ emacs/init.el
@@ -96,5 +96,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ricty Diminished for Powerline" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Ricty Diminished" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-ycmd ycmd company vimrc-mode flycheck molokai-theme mozc fiplr undo-tree))))
