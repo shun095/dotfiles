950fb504af8945e029061e9fa5cb6f8d681cacfb
ishitaku5522 <ishitaku5522@gmail.com>
2017-11-28 20:22:34 +0900

    custom of init el

--- emacs/init.el
+++ emacs/init.el
@@ -18,6 +18,7 @@
     restart-emacs
     magit
     yasnippet
+    yasnippet-snippets
     )
   "packages to be installed")
 
@@ -193,4 +194,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets vimrc-mode undo-tree restart-emacs mozc molokai-theme magit helm-package flycheck-irony company-jedi company-irony atom-dark-theme)))
  '(tool-bar-mode nil))
