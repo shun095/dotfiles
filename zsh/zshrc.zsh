51e59756a7dd77af16c62223acfaf4084750ca05
ishitaku5522 <ishitaku5522@gmail.com>
2018-11-01 13:28:09 +0900

    add vim to path

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,10 +6,13 @@ if [[ ! -n $TMUX ]]; then
     export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=59"
     export GOPATH=$HOME/.gopath
     export PATH=$HOME/usr/bin:$GOPATH/bin:/usr/local/go/bin:$PATH
+
     export PATH="$HOME/.pyenv/bin:$PATH"
-    export PATH="$HOME/build/nvim/bin:$PATH"
-    export PATH="$HOME/build/nvim-qt/bin:$PATH"
     export PATH="$HOME/build/ctags/bin:$PATH"
+    export PATH="$HOME/build/nvim-qt/bin:$PATH"
+    export PATH="$HOME/build/nvim/bin:$PATH"
+    export PATH="$HOME/build/tmux/bin:$PATH"
+    export PATH="$HOME/build/vim/bin:$PATH"
 fi
 
 if type pyenv > /dev/null; then
