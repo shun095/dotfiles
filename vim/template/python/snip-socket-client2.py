0af805de4187ecaf3eb2581277a5a2ee2f3cc36a
ishitaku5522 <ishitaku5522@gmail.com>
2019-01-19 12:15:44 +0900

    add socket template

---
+++ vim/template/python/snip-socket-client2.py
@@ -0,0 +1,12 @@
+# https://docs.python.org/ja/2.7/library/socket.html
+# Echo client program
+import socket
+
+HOST = 'daring.cwi.nl'    # The remote host
+PORT = 50007              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+data = s.recv(1024)
+s.close()
+print 'Received', repr(data)
