8c9b6f2b4b7b89664253460a2b74bbba064c3d76
ishitaku5522 <ishitaku5522@gmail.com>
2020-01-29 19:08:50 +0900

    fix trial script

--- trial.sh
+++ trial.sh
@@ -5,7 +5,5 @@
 #
 # Distributed under terms of the MIT license.
 
-git clone --depth 1 https://github.com/ishitaku5522/dotfiles
-cd ./dotfiles/
 docker build -t ishitaku_dotfiles .
-docker run -it --rm --name ishitaku_dotfiles ishitaku_dotfiles /bin/zsh
+docker run -it --rm --name ishitaku_dotfiles ishitaku_dotfiles /bin/zsh --login
