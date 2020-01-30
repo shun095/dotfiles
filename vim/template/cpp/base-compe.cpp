26e4101f40d145a031664af7253af65b1c79e30b
ishitaku5522 <ishitaku5522@gmail.com>
2020-01-30 14:43:26 +0000

    add competition programming template

---
+++ vim/template/cpp/base-compe.cpp
@@ -0,0 +1,17 @@
+/*
+ * {{_expr_:strftime('%Y-%m-%d')}} {{_name_}}.cpp
+ * Copyright © {{_expr_:strftime('%Y')}} ishitaku5522
+ * Distributed under terms of the MIT license.
+ */
+
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+
+int main(int argc, char const *argv[]) {
+  int n;{{_cursor_}}
+  cin >> n;
+  cout << n;
+  return 0;
+}
