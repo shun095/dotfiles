f589eaa496a9ab9358e89892838a505f2e5c016c
ishitaku5522 <ishitaku5522@gmail.com>
2021-12-17 21:52:34 +0900

    make git fetch shallower

--- tools/myconfigure_setup.sh
+++ tools/myconfigure_setup.sh
@@ -18,7 +18,7 @@ update_repository() {
 	local branch_name=$2
 	local needs_pull=$3
 	cd ${repo_dir}
-	git fetch --all -t -f
+	git fetch origin ${branch_name}
 	git checkout ${branch_name}
 
 	if ${needs_pull}; then
