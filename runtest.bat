5ec5c950fbb793dc6dcc634834fa3c1a5fdd8753
ishitaku5522 <ishitaku5522@gmail.com>
2021-12-30 19:22:10 +0900

    fix runtest.bat

--- runtest.bat
+++ runtest.bat
@@ -1,4 +1,5 @@
 @echo off
+set MYDOTFILES=%~dp0
 set VADER_OUTPUT_FILE=test_result.log
 
 call vim --not-a-term --cmd "let g:is_test = 1" --cmd "set shortmess=a cmdheight=10" -c "Vader! ./vim/test/myvimrc.vader"
