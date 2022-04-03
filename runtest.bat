8ad51fcbb9bdf3abc6155437c8a327cce42a3569
ishitaku5522 <ishitaku5522@gmail.com>
2022-04-03 23:05:29 +0900

    fix: fix runtest.bat

--- runtest.bat
+++ runtest.bat
@@ -2,7 +2,7 @@
 set MYDOTFILES=%~dp0
 set VADER_OUTPUT_FILE=test_result.log
 
-call vim --not-a-term --cmd "let g:is_test = 1" --cmd "set shortmess=a cmdheight=10" -c "Vader! ./vim/test/myvimrc.vader"
+call vim --not-a-term --cmd "let g:is_test = 1" --cmd "set shortmess=a cmdheight=10" --cmd "cal feedkeys(""\<CR>\<CR>\<CR>\<CR>\<CR>"""")" -c "Vader! ./vim/test/myvimrc.vader"
 
 set RC=%ERRORLEVEL%
 
