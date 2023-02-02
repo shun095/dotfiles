1e46cf75866d8695d4f99d82134729d8f51c95f9
shun095 <8069181+shun095@users.noreply.github.com>
2023-02-02 13:28:51 +0900

    update pyenv config (#23)

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -134,6 +134,11 @@ if [ $(uname) = "Darwin" ]; then
     alias excel='open -n /Applications/Microsoft\ Excel.app'
 fi
 
+if [ -d "$HOME/.pyenv" ]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+fi
 
 # source oh-my-zsh config
 source $MYDOTFILES/zsh/ohmyzshrc.zsh
