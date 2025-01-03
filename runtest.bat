2233858925b1c7c6603554330e4536a8e563ba54
ishitaku5522 <ishitaku5522@gmail.com>
2025-01-03 20:43:59 +0900

    try to change bat

--- runtest.bat
+++ runtest.bat
@@ -1,7 +1,7 @@
 @echo off
 set MYDOTFILES=%~dp0
 
-call vim --not-a-term --cmd "let g:is_test = 1" --cmd "set shortmess=a cmdheight=10" --cmd "cal feedkeys(""\<CR>\<CR>\<CR>\<CR>\<CR>"""")" -c ":PlugInstall --sync" -c ":qa!"
+call vim --not-a-term --cmd "let g:is_test = 1" --cmd "set shortmess=a cmdheight=10" --cmd "cal feedkeys(""\<CR>\<CR>\<CR>\<CR>\<CR>"")" -c ":PlugInstall --sync" -c ":qa!"
 
 %USERPROFILE%/vimfiles/plugged/vim-themis/bin/themis
 
