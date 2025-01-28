3f51f0cd21bd143314bc8c99eac7df52cb2293cc
ishitaku5522 <ishitaku5522@gmail.com>
2025-01-28 20:06:16 +0900

    update

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require('wezterm')
+local config = wezterm.config_builder()
+
+config.color_scheme = 'iceberg-dark'
+config.font = wezterm.font_with_fallback({
+    'JetBrainsMono Nerd Font',
+    'BIZ UDGothic',
+})
+config.font_size = 13
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+config.keys = {
+    { key = "f", mods = "CTRL|CMD", action = wezterm.action.ToggleFullScreen },
+}
+
+return config
