3d1c78ec067dfa2567d5caa9f60a47a509e78b31
ishitaku5522 <ishitaku5522@gmail.com>
2025-02-06 23:22:08 +0900

    update

--- vim/init.lua
+++ vim/init.lua
@@ -470,6 +470,7 @@ dap.configurations.lua = {
         name = "Attach to running Neovim instance",
     }
 }
+vim.fn.sign_define('DapBreakpoint', {text='🔴'})
 
 dap.adapters.nlua = function(callback, config)
     callback({ type = 'server', host = config.host or "127.0.0.1", port = config.port or 8086 })
