6cdbc55607f5da8a9b65395ffd21622f192ba653
ishitaku5522 <ishitaku5522@gmail.com>
2025-02-13 19:32:09 +0900

    update

--- runtest.bat
+++ runtest.bat
@@ -3,7 +3,7 @@ set MYDOTFILES=%~dp0
 
 luarocks --lua-version=5.1 --local install vusted
 
-set PATH=%APPDATA%/LuaRocks/bin;%PATH%
+set PATH=%APPDATA%/LuaRocks/bin;C:\tools\neovim\nvim-win64\bin;%PATH%
 for /f "delims=" %%i in ('luarocks --lua-version=5.1 --local path') do %%i
 echo PATH: %PATH%
 echo LUA_PATH: %LUA_PATH%
