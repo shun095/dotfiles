a1619b6b5621613941038fefb46f02e397304182
ishitaku5522 <ishitaku5522@gmail.com>
2025-02-17 17:30:22 +0900

    update

--- install.py
+++ install.py
@@ -7,8 +7,93 @@ from libs import utils
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
+def update():
+    # ディレクトリ構成にパッチを当てる
 
-def install()
+    # バックアップする
+
+    # システムのアップデート
+
+    # buildtools済みであればそちらも更新？
+
+    # 各種ツールアップデート
+    #   win:  fzf, oh-my-posh, oh-my-bash(git bash)
+    #   unix: fzf, oh-my-zsh, oh-my-bash, zsh plugin, tpm
+
+    # プラグインアップデート
+    #   win:  vim plugin, neovim plugin
+    #   unix: vim plugin, neovim plugin, tmux plugin
+
+    # パッチを当てる（脆弱性対応など）
+
+    # テストを回す？
+    pass
+
+
+def buildtools():
+    # ディレクトリ構成にパッチを当てる
+
+    # バックアップする
+
+    # 前提ライブラリのインストール
+
+    # 各種ツールのビルド、インストール
+    #   win:  vim, neovim, luajit?, luarocks?
+    #   unix: vim, neovim, tmux
+
+    # パッチを当てる（脆弱性対応など）
+
+    # テストを回す？
+    pass
+
+
+def test():
+    # 前提ライブラリのインストール
+
+    # テストの実行
+    pass
+
+
+def install():
+    # ディレクトリ構成にパッチを当てる?
+
+    # バックアップする
+
+    # git, vim, neovim, zsh, tmuxのインストール
+    #   win:  git, vim, neovim
+    #   unix: git, vim, neovim, zsh, tmux
+
+    # fzf, oh-my-zsh, oh-my-posh, oh-my-bash, zshプラグイン, tpmのダウンロード
+    #   win:  fzf, oh-my-posh, oh-my-bash(git bash)
+    #   unix: fzf, oh-my-zsh, oh-my-bash, zsh plugin, tpm
+
+    # zshテーマのインストール
+    #   win:  none
+    #   unix: ishitaku.zsh-theme
+
+    # RCファイルにsourceを追記（winがシンボリックリンク面倒なので、シンボリックリンクはやめる?）
+    #   win:  vimrc, gvimrc, init.lua, ginit.vim, tigrc, weztermrc, profile.ps1
+    #   unix: vimrc, gvimrc, init.lua, ginit.vim, tigrc, weztermrc, zshrc, bashrc, tmux.conf
+    # シンボリックリンクを辞めるデメリット: XDGディレクトリまとめてシンボリックリンク貼って終わり！ができない
+
+    # デフォルトのシェルの環境変数をまとめて定義
+    #   win: profile.ps1
+    #   unix: .profile, .zshenv
+
+    # tmux-256infoのterm infoを生成
+
+    # 各種プラグインインストール
+    #   win: fzf, vim plugin, neovim plugin, git config書き換え
+    #   win: fzf, vim plugin, neovim plugin, git config書き換え, tmux plugin
+
+    # パッチを当てる（脆弱性対応など）
+
+    # テストを回す？
+    pass
+
+
+def uninstall():
+    pass
 
 
 def main():
---
+++ patches/0.2.0.py
@@ -0,0 +1,45 @@
+print("hello world")
+
+# bak0→bakに変更 (source を書き込むファイルはバックアップを取っていない)
+# 対象:
+#   $HOME/.config/nvim/init.lua ※ winの場合別ディレクトリ
+#   $HOME/.config/nvim/ginit.vim ※ winの場合別ディレクトリ
+#   $HOME/.tigrc
+#   $HOME/.wezterm.lua
+
+# もしbakが存在したら削除
+#   $HOME/.zshrc
+#   $HOME/.vimrc
+#   $HOME/.gvimrc
+#   $HOME/.tmux.conf
+#
+#   $HOME/.config/nvim/init.vim
+#   $HOME/.config/flake8
+#   $HOME/.vintrc.yml
+#   $HOME/.spacemacs
+#   $HOME/.config/alacritty/alacritty.toml
+
+# 設定自体削除
+#   $HOME/.config/nvim/init.vim
+#   $HOME/.config/flake8
+#   $HOME/.vintrc.yml
+#   $HOME/.spacemacs
+#   $HOME/.config/alacritty/alacritty.toml
+
+# XDG化検討
+#   $HOME/.tmux.conf
+#   $HOME/.wezterm.lua
+#   $HOME/.zshrc
+#   $HOME/.vimrc
+#   $HOME/.gvimrc
+
+
+# localrcs→.localrcsに命名変更
+
+# .vimsessions命名変更
+
+# .cd_history削除 or 命名変更
+
+# .viminfoファイル見直し
+
+# zwcファイル削除
