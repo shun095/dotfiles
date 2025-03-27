d2fcbadda9c5232de0b4cd4dd579ca7d37f33c32
ishitaku5522 <ishitaku5522@gmail.com>
2025-03-28 02:36:08 +0900

    feat(vim/lua/plugins/minuet-ai.lua): update AI plugin configuration to use Llama.cpp
    - Switched endpoint from Ollama to Llama.cpp
    - Removed unused optional settings
    - Added new `template` section with custom prompt function and disabled suffix

--- vim/lua/plugins/minuet-ai.lua
+++ vim/lua/plugins/minuet-ai.lua
@@ -13,13 +13,37 @@ return {
                 openai_fim_compatible = {
                     api_key = 'TERM',
                     name = '🤖',
-                    end_point = 'http://localhost:11434/v1/completions',
-                    model = 'qwen2.5-coder:3b',
+
+                    -- Ollama configuration
+                    -- end_point = 'http://localhost:11434/v1/completions',
+                    -- model = 'qwen2.5-coder:3b',
+                    -- optional = {
+                    --     -- max_tokens = 20,
+                    --     -- top_p = 0.9,
+                    --     top_k = 20,
+                    --     temperature = 0.5,
+                    -- },
+
+                    -- Llama.cpp configuration
+                    end_point = 'http://localhost:8080/v1/completions',
+                    -- The model is set by the llama-cpp server and cannot be altered
+                    -- post-launch.
+                    model = 'llama',
                     optional = {
-                        -- max_tokens = 20,
-                        -- top_p = 0.9,
-                        top_k = 20,
                         temperature = 0.5,
+                    },
+                    -- Llama.cpp does not support the `suffix` option in FIM completion.
+                    -- Therefore, we must disable it and manually populate the special
+                    -- tokens required for FIM completion.
+                    template = {
+                        prompt = function(context_before_cursor, context_after_cursor)
+                            return '<|fim_prefix|>'
+                                .. context_before_cursor
+                                .. '<|fim_suffix|>'
+                                .. context_after_cursor
+                                .. '<|fim_middle|>'
+                        end,
+                        suffix = false,
                     },
                 },
             },
