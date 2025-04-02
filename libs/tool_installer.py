653dbca4c0af68da1e3cc12ccbc2f11dc619272d
ishitaku5522 <ishitaku5522@gmail.com>
2025-04-02 20:56:32 +0900

    wip: upload wrong code generated by AI

--- libs/tool_installer.py
+++ libs/tool_installer.py
@@ -1,3 +1,14 @@
+import os
 
-def install():
-    pass
+class ToolInstaller:
+    @staticmethod
+    def select_package_manager():
+        if os.name == 'posix' and 'darwin' in os.uname().sysname.lower():
+            return 'brew'
+        elif os.name == 'posix':
+            return 'apt-get'
+        elif os.name == 'nt':
+            return 'winget'
+        else:
+            raise ValueError("Unsupported operating system")
+
--- libs/tool_installer_test.py
+++ libs/tool_installer_test.py
@@ -1,13 +1,38 @@
-from . import tool_installer
 from pyfakefs import fake_filesystem_unittest
-
-# apt, brew, scoop, dnfなどを使ってソフトウェアをインストールする
-# - 適切なパッケージマネージャを選ぶ
-# - パッケージマネージャで入れたいコマンドに対応するパッケージを探す
+import unittest
+import os
+from libs.tool_installer import ToolInstaller
 
 class Test_get_deployed_version(fake_filesystem_unittest.TestCase):
     def setUp(self):
         self.setUpPyfakefs()
 
-    # def test_(self):
-    #    tool_installer.install()
+class TestToolInstaller(unittest.TestCase):
+
+    def test_select_package_manager_darwin(self):
+        with self.assertRaises(ValueError):
+            ToolInstaller.select_package_manager()
+        self.mock_os_uname = self.mock.create_attribute(os, 'uname', return_value=(None, 'Darwin', '19', '', ''))
+        self.assertEqual(ToolInstaller.select_package_manager(), 'brew')
+
+    def test_select_package_manager_linux(self):
+        with self.assertRaises(ValueError):
+            ToolInstaller.select_package_manager()
+        self.mock_os_uname = self.mock.create_attribute(os, 'uname', return_value=(None, 'Linux', '5', '', ''))
+        self.assertEqual(ToolInstaller.select_package_manager(), 'apt-get')
+
+    def test_select_package_manager_windows(self):
+        with self.assertRaises(ValueError):
+            ToolInstaller.select_package_manager()
+        self.mock_os_uname = self.mock.create_attribute(os, 'uname', return_value=(None, 'NT', '10', '', ''))
+        self.assertEqual(ToolInstaller.select_package_manager(), 'winget')
+
+    def test_select_package_manager_unsupported(self):
+        with self.assertRaises(ValueError):
+            ToolInstaller.select_package_manager()
+        self.mock_os_uname = self.mock.create_attribute(os, 'uname', return_value=(None, 'Unknown', '1', '', ''))
+        with self.assertRaises(ValueError):
+            ToolInstaller.select_package_manager()
+
+if __name__ == '__main__':
+    unittest.main()
