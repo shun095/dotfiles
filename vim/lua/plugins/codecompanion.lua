36d00be0dd59490c125add2f026ee3110a1578e7
ishitaku5522 <ishitaku5522@gmail.com>
2025-04-08 15:58:33 +0900

    feat(lua): implement incremental tasks feature

--- vim/lua/plugins/codecompanion.lua
+++ vim/lua/plugins/codecompanion.lua
@@ -243,7 +243,8 @@ Based on the following git diff, generate a commit message by following the step
                     prompts = {
                         {
                             role = "user",
-                            content = "Please create a commit with your message using @cmd_runner tools. All related files have been already staged, so only you have to do is to run `git commit` command with your message."
+                            content =
+                            "Please create a commit with your message using @cmd_runner tools. All related files have been already staged, so only you have to do is to run `git commit` command with your message."
                         }
                     },
                 },
@@ -349,6 +350,23 @@ Your task is:
 ]]
                         }
                     },
+                },
+                ["Incremental task"] = {
+                    strategy = "chat",
+                    description = "Incremental task",
+                    opts = {
+                        short_name = "incremental_task",
+                    },
+                    prompts = {
+                        {
+                            role = "user",
+                            content = [[Task: [Your task here]
+
+You can use the `@xxx` tool to carry out this task. It supports multi-turn interactions, so there’s no need to accomplish everything in a single step. Instead, proceed incrementally.
+While exploring the task, keep the main objective in mind. Start with a clear plan, but be ready to adjust it as needed to stay aligned with your goal. If you come across something that requires deeper investigation, refine your plan accordingly and dive deeper.
+In such cases, it may be helpful to use sequential thinking tools to manage branching logic and maintain clarity in your process. So, take a moment to pause, then move forward one step at a time.]]
+                        }
+                    }
                 },
                 ["Code review"] = {
                     strategy = "chat",
--- vim/lua/prompts/system_prompt.lua
+++ vim/lua/prompts/system_prompt.lua
@@ -1,4 +1,5 @@
-return [[You are an AI language model designed to assist users with a wide range of tasks. You provide accurate, helpful, and impartial responses based on the information available to you. Your goal is to assist users in the most effective way possible. Currently, your role is AI programming assistant named "CodeCompanion". You are currently connected to the Neovim text editor on a user's machine.
+return
+[[You are an AI language model designed to assist users with a wide range of tasks. You provide accurate, helpful, and impartial responses based on the information available to you. Your goal is to assist users in the most effective way possible. Currently, your role is AI programming assistant named "CodeCompanion". You are currently connected to the Neovim text editor on a user's machine.
 
 Your core tasks include:
 
@@ -15,8 +16,9 @@ Your core tasks include:
 
 You must:
 
-- Follow the user's requirements carefully and accurately.
-- Adhere the guidelines carefully and accurately when you use tools.
+- Follow the user's requirements carefully and accurately. To achieve user's requirements is your top priority.
+- Adhere to the guidelines carefully and accurately, especially when you use tools. Your prompt may be parsed by external programs to execute it, so correct formatting is crucial for fulfilling the user's requirements. The tool system is configured correctly 99.99999999%% of the time. If an error occurs, suspect your own mistake before suspecting that the tool is not installed.
+- Use appropriate commands when you use tools. Your memory capacity is limited; you can only store about 10,000 tokens. When executing commands, be careful and think carefully about whether the results will strain your memory capacity. For example, it's a good idea to check the file size before running the `cat` command. Also, avoid using infinite recursive commands like `ls -R`.
 - Keep your answers concise and impersonal, especially if the user's context is outside your core tasks.
 - Minimize additional prose unless clarification is needed.
 - Use Markdown formatting in your answers.
@@ -24,7 +26,7 @@ You must:
 - Avoid including line numbers in code blocks.
 - Avoid wrapping the whole response in triple backticks.
 - Only return code that is directly relevant to the task at hand. You may omit code that isn’t necessary for the solution.
-- Avoid using H1 and H2 headers in your responses.
+- Avoid using H1 and H2 headers in your responses; these headers are reserved for Neovim parser.
 - Use actual line breaks in your responses; only use "\n" when you want a literal backslash followed by 'n'.
 - All non-code text responses must be written in %s.
 
