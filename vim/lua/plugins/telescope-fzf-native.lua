15b7c9214810f4bdf7b4563bd526b3269d5fd7be
ishitaku5522 <ishitaku5522@gmail.com>
2025-09-23 17:12:02 +0900

    build(telescope-fzf-native): add CMake policy version minimum requirement
    - Introduced `-DCMAKE_POLICY_VERSION_MINIMUM=3.5` to ensure compatibility with specific CMake versions
    
    This ensures that the build process uses a minimum CMake policy version of 3.5, addressing potential incompatibilities with older CMake configurations.

--- vim/lua/plugins/telescope-fzf-native.lua
+++ vim/lua/plugins/telescope-fzf-native.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release',
     dependencies = {
         "nvim-telescope/telescope.nvim",
     },
